55e2159465f1c8e9f175fdde27cecd20de0baf8d
Proteus Qian <sudaku233@outlook.com>
2025-08-20 11:15:18 +0800

    feat: VPS: enhance NixOS hardware and ZFS configurations
    - custom_files/config.json.age: Updated encrypted proxy configuration
    - machines/x86_64-linux/Proteus-NUC/hardware-configuration.nix: Removed unused lib parameter
    - machines/x86_64-linux/Proteus-NixOS-1/hardware-configuration.nix: Replaced ext4 with ZFS for root, home, nix, and root filesystems, updated kernel modules, added swap and ZFS settings
    - machines/x86_64-linux/Proteus-NixOS-1/zfs.nix: Added configuration to select latest ZFS-compatible kernel
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- machines/x86_64-linux/Proteus-NUC/hardware-configuration.nix
+++ machines/x86_64-linux/Proteus-NUC/hardware-configuration.nix
@@ -1,4 +1,4 @@
-{config, lib, modulesPath, ...}: {
+{config, modulesPath, ...}: {
   imports = [(modulesPath + "/installer/scan/not-detected.nix")];
 
   boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "uas" "sd_mod"];
--- machines/x86_64-linux/Proteus-NixOS-1/hardware-configuration.nix
+++ machines/x86_64-linux/Proteus-NixOS-1/hardware-configuration.nix
@@ -1,31 +1,38 @@
-{config, lib, pkgs, modulesPath, ...}: {
+{modulesPath, ...}: {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" ];
+  # boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "virtio_pci" "uhci_hcd" "ehci_pci" "ahci" "usbhid" "sr_mod" "virtio_blk"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
+  boot.resumeDevice = "/dev/disk/by-partlabel/swap\\x20partition";
+  boot.zfs.forceImportRoot = false;
+  boot.zfs.allowHibernation = true; # Make sure not use Swap on ZFS
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/a4fb86f7-6719-4263-ab8f-648b4fa57ce7";
-    fsType = "ext4";
-    options = ["discard"];
+  swapDevices = [{device = "/dev/disk/by-partlabel/swap\\x20partition";}];
+  # Disable zfs-mount, use NixOS systemd mount management
+  # Ref: https://wiki.nixos.org/wiki/ZFS#ZFS_conflicting_with_systemd
+  systemd.services.zfs-mount.enable = false;
+
+  fileSystems = {
+    "/" = {device = "zroot/default"; fsType = "zfs"; options = ["zfsutil"];};
+    "/home" = {device = "zroot/home"; fsType = "zfs"; options = ["zfsutil"];};
+    "/nix" = {device = "zroot/nix"; fsType = "zfs"; options = ["zfsutil"];};
+    "/root" = {device = "zroot/home/root"; fsType = "zfs"; options = ["zfsutil"];};
+    "/boot" = {
+      device = "/dev/disk/by-partlabel/EFI\\x20system\\x20partition";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
   };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/D24A-7810";
-    fsType = "vfat";
-    options = ["discard" "fmask=0022" "dmask=0022"];
-  };
-
-  swapDevices = [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = true;
-  # networking.interfaces.ens3.useDHCP = true;
+  networking.hostId = "14cb080c"; # ZFS requires this
 
   nixpkgs.hostPlatform = "x86_64-linux";
 }
