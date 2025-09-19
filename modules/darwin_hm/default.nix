7bc5dfefa38f69b3ca57b57053bf0198f554084d
Proteus Qian <sudaku233@outlook.com>
2025-09-20 07:18:39 +0800

    feat: remove webm file association for mpv
    - modules/darwin_hm/default.nix: Removed .webm file association for mpv
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- modules/darwin_hm/default.nix
+++ modules/darwin_hm/default.nix
@@ -30,7 +30,6 @@
     # Set file extensions
     ${lib.getExe' pkgs.duti "duti"} -s io.mpv .mkv viewer
     ${lib.getExe' pkgs.duti "duti"} -s io.mpv .mp4 viewer
-    ${lib.getExe' pkgs.duti "duti"} -s io.mpv .webm viewer
   '');
   ## END associations.nix
   ## START syncthing.nix
