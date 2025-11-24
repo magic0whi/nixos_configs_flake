51b2abd7062c2cd9b54f02ef78dba21c338db0dd
Proteus Qian <sudaku233@outlook.com>
2025-11-24 15:01:08 +0800

    refactor(services): move tor config from stew.nix to Proteus-NUC and include lib binding
    - Moved tor.nix configuration from modules/nixos_headless/stew.nix to machines/x86_64-linux/Proteus-NUC/services.nix
    - Added lib argument to the function header
    - Updated binary config.json.age
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- machines/x86_64-linux/Proteus-NUC/services.nix
+++ machines/x86_64-linux/Proteus-NUC/services.nix
@@ -1,8 +1,27 @@
-{pkgs, mylib, config, myvars, ...}: {
+{pkgs, lib, mylib, config, myvars, ...}: {
   # networking.firewall.extraInputRules = ''
   #   tcp dport ldaps accept comment "Allow OpenLDAP"
   #   udp dport ldaps accept comment "Allow OpenLDAP"
   # '';
+  ## START tor.nix
+  services.tor = {
+    enable = true;
+    client.enable = true;
+    # openFirewall = true;
+    settings = {
+      ExitNodes = "{GB}";
+      ExitPolicy = ["accept *:*"];
+      AvoidDiskWrites = 1;
+      HardwareAccel = 1;
+      UseBridges = true;
+      ClientTransportPlugin = "snowflake exec ${lib.getExe' pkgs.snowflake "client"} -url https://snowflake-broker.azureedge.net/ -front ajax.aspnetcdn.com -ice stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn -log /tmp/snowflake-client.log";
+      Bridge = [
+        "snowflake 192.0.2.3:80 2B280B23E1107BB62ABFC40DDCC8824814F80A72 fingerprint=2B280B23E1107BB62ABFC40DDCC8824814F80A72 url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn"
+        "snowflake 192.0.2.4:80 8838024498816A039FCBBAB14E6F40A0843051FA fingerprint=8838024498816A039FCBBAB14E6F40A0843051FA url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.net:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn"
+      ];
+    };
+  };
+  ## END tor.nix
   services.openldap = {
     enable = true;
     urlList = ["ldaps:///"];
--- modules/nixos_headless/stew.nix
+++ modules/nixos_headless/stew.nix
@@ -281,44 +281,4 @@
     };
   };
   ## END security.nix
-  ## START tor.nix
-  services.tor = {
-    enable = true;
-    client.enable = true;
-    # openFirewall = true;
-    settings = {
-      ExitNodes = "{GB}";
-      ExitPolicy = ["accept *:*"];
-      AvoidDiskWrites = 1;
-      HardwareAccel = 1;
-      UseBridges = true;
-      ClientTransportPlugin = "snowflake exec ${lib.getExe' pkgs.snowflake "client"} -url https://snowflake-broker.azureedge.net/ -front ajax.aspnetcdn.com -ice stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn -log /tmp/snowflake-client.log";
-      Bridge = [
-        "snowflake 192.0.2.3:80 2B280B23E1107BB62ABFC40DDCC8824814F80A72 fingerprint=2B280B23E1107BB62ABFC40DDCC8824814F80A72 url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn"
-        "snowflake 192.0.2.4:80 8838024498816A039FCBBAB14E6F40A0843051FA fingerprint=8838024498816A039FCBBAB14E6F40A0843051FA url=https://1098762253.rsc.cdn77.org/ fronts=www.cdn77.com,www.phpmyadmin.net ice=stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.net:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478 utls-imitate=hellorandomizedalpn"
-      ];
-    };
-  };
-  ## END tor.nix
-  ## START i2pd.nix
-  services.i2pd = {
-    enable = true;
-    enableIPv6 = true;
-    upnp.enable = true;
-    reseed = {
-      verify = true;
-      proxy = "socks://127.0.0.1:2080";
-      urls = [
-        "https://reseed.i2p-projekt.de/"
-        "https://i2p.mooo.com/netDb/"
-        "https://netdb.i2p2.no/"
-      ];
-    };
-    proto = {
-      http.enable = true;
-      httpProxy.enable = true;
-      socksProxy.enable = true;
-    };
-  };
-  ## END i2pd.nix
 }
