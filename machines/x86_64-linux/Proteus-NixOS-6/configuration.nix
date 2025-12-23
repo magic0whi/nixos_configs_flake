4da2dd8a558f8b0f5fec1b3e94d8a2e89759c481
Proteus Qian <sudaku233@outlook.com>
2025-12-23 19:32:27 +0800

    fix(sing-box): use correct config for Proteus-NixOS-6
    - Change from `sb_Proteus-NixOS-1.json` to `sb_Proteus-NixOS-6.json` secret.
    - Update `services.sing-box.config_file` to reference the node-specific age secret.
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- machines/x86_64-linux/Proteus-NixOS-6/configuration.nix
+++ machines/x86_64-linux/Proteus-NixOS-6/configuration.nix
@@ -12,13 +12,13 @@
   ];
   services.syncthing.enable = false;
   ## START sing-box.nix
-  age.secrets."sb_Proteus-NixOS-1.json" = {
-    file = "${myvars.secrets_dir}/sb_Proteus-NixOS-1.json.age";
+  age.secrets."sb_Proteus-NixOS-6.json" = {
+    file = "${myvars.secrets_dir}/sb_Proteus-NixOS-6.json.age";
     mode = "0000"; owner = "root";
   };
   networking.firewall = {allowedTCPPorts = [443];}; # Reality
   services.sing-box.enable = true;
-  services.sing-box.config_file = config.age.secrets."sb_Proteus-NixOS-1.json".path;
+  services.sing-box.config_file = config.age.secrets."sb_Proteus-NixOS-6.json".path;
   ## END sing-box.nix
   boot.binfmt.emulatedSystems = ["riscv64-linux"]; # Cross compilation
 }
