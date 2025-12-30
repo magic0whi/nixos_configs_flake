396576b10bf8b8401f5c8ccb3b81ae89fc5786e4
Proteus Qian <sudaku233@outlook.com>
2025-12-30 09:15:48 +0800

    refactor(zellij): move config from secrets file into Home Manager
    - Inline Zellij plugins and full keybinds configuration in `shell.nix`.
    - Drop `myvars` argument and remove the unused `xdg.configFile` source hook.
    - Delete `secrets/zellij.kdl`, since that config is managed declaratively.
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- modules/common_hm_headless/shell.nix
+++ modules/common_hm_headless/shell.nix
@@ -1,4 +1,4 @@
-{config, pkgs, lib, myvars, ...}: {
+{config, pkgs, lib, ...}: {
   home.packages = with pkgs; [
     tlrc # tldr written in Rust
     fd # search for files by name, faster than find
@@ -17,26 +17,54 @@
       keybinds = {
         _props.clear-defaults = true;
         _children = [
-          {
-            shared_except = {
-              _args = ["locked"];
-              _children = [
-                {bind = {_args = ["Ctrl g"]; SwitchToMode = "Locked";};}
-                {bind = {_args = ["Ctrl q"]; Quit = {};};}
-                {bind = {_args = ["Alt n"]; NewPane = {};};}
-                {bind = {_args = ["Alt h" "Alt Left"]; MoveFocusOrTab = "Left";};}
-                {bind = {_args = ["Alt j" "Alt Down"]; MoveFocusOrTab = "Down";};}
-                # TODO
-              ];
-            };
-          }
-          {
-            shared_except = {
-              _args = ["normal" "locked"];
-              bind = {_args = ["Enter" "Esc"]; SwitchToMode = "Locked";};
-            };
-          }
-          # TODO
+          {shared_except = {
+            _args = ["locked"];
+            _children = [
+              {bind = {_args = ["Ctrl g"]; SwitchToMode = "Locked";};}
+              {bind = {_args = ["Ctrl q"]; Quit = {};};}
+              {bind = {_args = ["Alt n"]; NewPane = {};};}
+              {bind = {_args = ["Alt h" "Alt Left"]; MoveFocusOrTab = "Left";};}
+              {bind = {_args = ["Alt j" "Alt Down"]; MoveFocus = "Down";};}
+              {bind = {_args = ["Alt k" "Alt Up"]; MoveFocus = "Up";};}
+              {bind = {_args = ["Alt l" "Alt Right"]; MoveFocusOrTab = "Right";};}
+              {bind = {_args = ["Alt =" "Alt +"]; Resize = "Increase";};}
+              {bind = {_args = ["Alt -" "Alt _"]; Resize = "Decrease";};}
+              {bind = {_args = ["Alt ["]; PreviousSwapLayout = {};};}
+              {bind = {_args = ["Alt ]"]; NextSwapLayout = {};};}
+            ];
+          };}
+          {shared_except = {
+            _args = ["normal" "locked"];
+            bind = {_args = ["Enter" "Esc"]; SwitchToMode = "Normal";};
+          };}
+          {shared_except = {
+            _args = ["pane" "locked"];
+            bind = {_args = ["Ctrl p"]; SwitchToMode = "Pane";};
+          };}
+          {shared_except = {
+            _args = ["resize" "locked"];
+            bind = {_args = ["Ctrl n"]; SwitchToMode = "Resize";};
+          };}
+          {shared_except = {
+            _args = ["scroll" "locked"];
+            bind = {_args = ["Ctrl s"]; SwitchToMode = "Scroll";};
+          };}
+          {shared_except = {
+            _args = ["session" "locked"];
+            bind = {_args = ["Ctrl o"]; SwitchToMode = "Session";};
+          };}
+          {shared_except = {
+            _args = ["tab" "locked"];
+            bind = {_args = ["Ctrl t"]; SwitchToMode = "Tab";};
+          };}
+          {shared_except = {
+            _args = ["move" "locked"];
+            bind = {_args = ["Ctrl h"]; SwitchToMode = "Move";};
+          };}
+          # {shared_except = {
+          #   _args = ["tmux" "locked"];
+          #   bind = {_args = ["Ctrl b"]; SwitchToMode = "Tmux";};
+          # };}
         ];
         # Uncomment this and adjust key if using copy_on_select=false
         # normal.bind = {_args = ["Alt c"]; Copy = {};};
@@ -98,29 +126,196 @@
             _children = [{SwitchToMode = "RenamePane";}{PaneNameInput = 0;}];
           };}
         ];
-      # pane._children = [
-      #   {
-      #     bind = {
-      #       _args = ["e"];
-      #       _children = [
-      #         { TogglePaneEmbedOrFloating = {}; }
-      #         { SwitchToMode._args = ["locked"]; }
-      #       ];
-      #     };
-      #   }
-      #   {
-      #     bind = {
-      #       _args = ["left"];
-      #       MoveFocus = ["left"];
-      #     };
-      #   }
-      # ];
-      # TODO
+        move._children = [
+          {bind = {_args = ["Ctrl h"]; SwitchToMode = "Normal";};}
+          {bind = {_args = ["n" "Tab"]; MovePane = {};};}
+          {bind = {_args = ["p"]; MovePaneBackwards = {};};}
+          {bind = {_args = ["h" "Left"]; MovePane = "Left";};}
+          {bind = {_args = ["j" "Down"]; MovePane = "Down";};}
+          {bind = {_args = ["k" "Up"]; MovePane = "Up";};}
+          {bind = {_args = ["l" "Right"]; MovePane = "Right";};}
+        ];
+        tab._children = [
+          {bind = {_args = ["Ctrl t"]; SwitchToMode = "Normal";};}
+          {bind = {
+            _args = ["r"];
+            _children = [{SwitchToMode = "RenameTab";}{TabNameInput = 0;}];
+          };}
+          {bind = {_args = ["h" "Left" "k" "Up"]; GoToPreviousTab = {};};}
+          {bind = {_args = ["l" "Right" "j" "Down"]; GoToNextTab = {};};}
+          {bind = {
+            _args = ["n"];
+            _children = [{NewTab = {};}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["x"];
+            _children = [{CloseTab = {};}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["s"];
+            _children = [{ToggleActiveSyncTab = {};}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["1"];
+            _children = [{GoToTab = 1;}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["2"];
+            _children = [{GoToTab = 2;}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["3"];
+            _children = [{GoToTab = 3;}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["4"];
+            _children = [{GoToTab = 4;}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["5"];
+            _children = [{GoToTab = 5;}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["6"];
+            _children = [{GoToTab = 6;}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["7"];
+            _children = [{GoToTab = 7;}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["8"];
+            _children = [{GoToTab = 8;}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["9"];
+            _children = [{GoToTab = 9;}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {_args = ["Tab"]; ToggleTab = {};};}
+        ];
+        scroll._children = [
+          {bind = {_args = ["Ctrl s"]; SwitchToMode = "Normal";};}
+          {bind = {
+            _args = ["e"];
+            _children = [{EditScrollback = {};}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {
+            _args = ["s"];
+            _children = [{SwitchToMode = "EnterSearch";}{SearchInput = 0;}];
+          };}
+          {bind = {
+            _args = ["Ctrl c"];
+            _children = [{ScrollToBottom = {};}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {_args = ["j" "Down"]; ScrollDown = {};};}
+          {bind = {_args = ["k" "Up"]; ScrollUp = {};};}
+          {bind = {_args = ["Ctrl f" "PageDown" "l" "Right"]; PageScrollDown = {};};}
+          {bind = {_args = ["Ctrl b" "PageUp" "h" "Left"]; PageScrollUp = {};};}
+          {bind = {_args = ["d" "Ctrl d"]; HalfPageScrollDown = {};};}
+          {bind = {_args = ["u" "Ctrl u"]; HalfPageScrollUp = {};};}
+          # Uncomment this and adjust key if using copy_on_select=false
+          # {bind = {_args = ["Alt c"]; Copy = {};};}
+        ];
+        search._children = [
+          {bind = {_args = ["Ctrl s"]; SwitchToMode = "Normal";};}
+          {bind = {
+            _args = ["Ctrl c"];
+            _children = [{ScrollToBottom = {};}{SwitchToMode = "Normal";}];
+          };}
+          {bind = {_args = ["j" "Down"]; ScrollDown = {};};}
+          {bind = {_args = ["k" "Up"]; ScrollUp = {};};}
+          {bind = {_args = ["Ctrl f" "PageDown" "l" "Right"]; PageScrollDown = {};};}
+          {bind = {_args = ["Ctrl b" "PageUp" "h" "Left"]; PageScrollUp = {};};}
+          {bind = {_args = ["d" "Ctrl d"]; HalfPageScrollDown = {};};}
+          {bind = {_args = ["u" "Ctrl u"]; HalfPageScrollUp = {};};}
+          {bind = {_args = ["n"]; Search = "Down";};}
+          {bind = {_args = ["N"]; Search= "Up";};}
+          {bind = {_args = ["c"]; SearchToggleOption = "CaseSensitivity";};}
+          {bind = {_args = ["w"]; SearchToggleOption = "Wrap";};}
+          {bind = {_args = ["o"]; SearchToggleOption = "WholeWord";};}
+        ];
+        entersearch._children = [
+          {bind = {_args = ["Ctrl c" "Esc"]; SwitchToMode = "Scroll";};}
+          {bind = {_args = ["Enter"]; SwitchToMode = "Search";};}
+        ];
+        renametab._children = [
+          {bind = {_args = ["Ctrl c"]; SwitchToMode = "Normal";};}
+          {bind = {
+            _args = ["Esc"];
+            _children = [{UndoRenameTab = {};}{SwitchToMode = "Tab";}];
+          };}
+        ];
+        renamepane._children = [
+          {bind = {_args = ["Ctrl c"]; SwitchToMode = "Normal";};}
+          {bind = {
+            _args = ["Esc"];
+            _children = [{UndoRenamePane = {};}{SwitchToMode = "Pane";}];
+          };}
+        ];
+        session._children = [
+          {bind = {_args = ["Ctrl o"]; SwitchToMode = "Normal";};}
+          {bind = {_args = ["Ctrl s"]; SwitchToMode = "Scroll";};}
+          {bind = {_args = ["d"]; Detach = {};};}
+        ];
+        # tmux._children = [
+        #   {bind = {_args = ["["]; SwitchToMode = "Scroll";};}
+        #   {bind = {
+        #     _args = ["Ctrl b"];
+        #     _children = [{Write = 2;}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {
+        #     _args = ["\""];
+        #     _children = [{NewPane = "Down";}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {
+        #     _args = ["%"];
+        #     _children = [{NewPane = "Right";}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {
+        #     _args = ["z"];
+        #     _children = [{ToggleFocusFullscreen = {};}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {
+        #     _args = ["c"];
+        #     _children = [{NewTab = {};}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {_args = [","]; SwitchToMode = "RenameTab";};}
+        #   {bind = {
+        #     _args = ["p"];
+        #     _children = [{GoToPreviousTab = {};}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {
+        #     _args = ["n"];
+        #     _children = [{GoToNextTab = {};}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {
+        #     _args = ["h" "Left"];
+        #     _children = [{MoveFocus = "Left";}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {
+        #     _args = ["j" "Down"];
+        #     _children = [{MoveFocus = "Down";}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {
+        #     _args = ["k" "Up"];
+        #     _children = [{MoveFocus = "Up";}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {
+        #     _args = ["l" "Right"];
+        #     _children = [{MoveFocus = "Right";}{SwitchToMode = "Normal";}];
+        #   };}
+        #   {bind = {_args = ["o"]; FocusNextPane = {};};}
+        #   {bind = {_args = ["d"]; Detach = {};};}
+        #   {bind = {_args = ["Space"]; NextSwapLayout = {};};}
+        #   {bind = {
+        #     _args = ["x"];
+        #     _children = [{CloseFocus = {};}{SwitchToMode = "Normal";}];
+        #   };}
+        # ];
       };
     };
   };
   home.shellAliases."zj" = "zellij";
-  # xdg.configFile."zellij/config.kdl".source = "${myvars.secrets_dir}/zellij.kdl"; # TODO
   ## END zellij.nix
 
   home.sessionVariables = { # Environment variables that always set at login
