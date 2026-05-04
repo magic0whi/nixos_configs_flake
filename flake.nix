9fc068a43c1b247d90505b7bbcbc50de32294f19
Proteus Qian <sudaku233@outlook.com>
2026-05-04 13:08:11 +0100

    ⬆️ build(flake): update nixpkgs, home-manager, nix-darwin, and i915-sriov-dkms pins
    - bump `nixpkgs` to `15f4ee4` (2026-05-04, nixos-unstable)
    - bump `home-manager` to `c909892` (2026-05-04, master)
    - bump `nix-darwin` to `8c62fba` (2026-05-04)
    - bump `i915-sriov-dkms` from tag `2026.03.05` to `2026.05.03`
    - consolidate multi-line `inputs.foo.follows` assignments into single `inputs = { ... }` attrsets for `lanzaboote`, `impermanence`, `agenix`, `deploy-rs`
    - inline single-attribute inputs onto one line for `pgp2ssh`, `sops-nix`, `catppuccin`, `disko`, `nix-darwin`, `i915-sriov-dkms`
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   description = "Proteus Qian's nix configuration for NixOS & nix-darwin";
   inputs = {
-    # Pinned as of 2026-04-14 10:49, branch: nixos-unstable
-    nixpkgs.url = "github:NixOS/nixpkgs/4c1018dae018162ec878d42fec712642d214fdfa";
-    # Pinned as of 2026-04-05 17:10, branch: nixos-unstable
-    # nixpkgs-postgresql.url = "github:NixOS/nixpkgs/6201e203d09599479a3b3450ed24fa81537ebc4e";
-    home-manager = { # Pinned as of 2026-04-14 10:49, branch: master
-      url = "github:nix-community/home-manager/2de7205ce6e10b031151033e69b7ef89708dc282";
+    # Pinned as of 2026-05-04 17:55, branch: nixos-unstable
+    nixpkgs.url = "github:NixOS/nixpkgs/15f4ee454b1dce334612fa6843b3e05cf546efab";
+    # Pinned as of 2026-04-14 17:55, branch: nixos-unstable
+    # nixpkgs-postgresql.url = "github:NixOS/nixpkgs/4c1018dae018162ec878d42fec712642d214fdfa";
+    home-manager = { # Pinned as of 2026-05-04 17:55, branch: master
+      url = "github:nix-community/home-manager/c909892de502b4de9e92838a503c09a9c8ebe4aa";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with the
       # `inputs.nixpkgs` of the current flake, to avoid problems caused by
@@ -15,14 +15,12 @@
     };
     lanzaboote = { # Pinned as of 2026-04-07 06:58
       url = "github:nix-community/lanzaboote/v1.0.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.rust-overlay.url = "github:oxalica/rust-overlay/stable";
+      inputs = {nixpkgs.follows = "nixpkgs"; rust-overlay.url = "github:oxalica/rust-overlay/stable";};
     };
     # Pinned as of 2026-04-07 06:53
     impermanence = {
       url = "github:nix-community/impermanence/7b1d382faf603b6d264f58627330f9faa5cba149";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
+      inputs = {nixpkgs.follows = "nixpkgs"; home-manager.follows = "home-manager";};
     };
     nixpak = { # Pinned as of 2026-04-07 06:54
       url = "github:nixpak/nixpak/4f8cbe437ba7e047ed4582b35b8140124b9562b5";
@@ -33,40 +31,33 @@
       # url = "github:ryantm/agenix/fcdea223397448d35d9b31f798479227e80183f6";
       # replaced with a type-safe reimplementation to get a better error message and less bugs.
       # url = "github:ryan4yin/ragenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-      inputs.systems.follows = "deploy-rs/utils/systems";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+        systems.follows = "deploy-rs/utils/systems";
+      };
     };
     pgp2ssh = { # Pinned as of 2026-04-07 06:54
-      url = "github:pinpox/pgp2ssh/792e3a3f107e6b4da7b96ded5d46b69efc45d8c1";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:pinpox/pgp2ssh/792e3a3f107e6b4da7b96ded5d46b69efc45d8c1"; inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = { # Pinned as of 2026-04-14 10:53
-      url = "github:Mic92/sops-nix/d4971dd58c6627bfee52a1ad4237637c0a2fb0cd";
-      inputs.nixpkgs.follows = "nixpkgs";};
+      url = "github:Mic92/sops-nix/d4971dd58c6627bfee52a1ad4237637c0a2fb0cd"; inputs.nixpkgs.follows = "nixpkgs";
+    };
     deploy-rs = { # Pinned as of 2026-04-07 06:56
       url = "github:serokell/deploy-rs/77c906c0ba56aabdbc72041bf9111b565cdd6171";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-compat.follows = "lanzaboote/pre-commit/flake-compat";
+      inputs = {nixpkgs.follows = "nixpkgs"; flake-compat.follows = "lanzaboote/pre-commit/flake-compat";};
     };
-    nix-darwin = { # Pinned as of 2026-04-14 10:55
-      url = "github:nix-darwin/nix-darwin/06648f4902343228ce2de79f291dd5a58ee12146";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = { # Pinned as of 2026-05-04 15:06
+      url = "github:nix-darwin/nix-darwin/8c62fba0854ba15c8917aed18894dbccb48a3777"; inputs.nixpkgs.follows = "nixpkgs";
     };
-    catppuccin = { # Pinned as of 2026-04-07 06:59, tag v25.11
-      url = "github:catppuccin/nix/v25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = { # Pinned as of 2026-04-07 06:59, tag v1.13.0
-      url = "github:nix-community/disko/v1.13.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Pinned as of 2026-04-07 06:59, tag v25.11
+    catppuccin = {url = "github:catppuccin/nix/v25.11"; inputs.nixpkgs.follows = "nixpkgs";};
+    # Pinned as of 2026-04-07 06:59, tag v1.13.0
+    disko = {url = "github:nix-community/disko/v1.13.0"; inputs.nixpkgs.follows = "nixpkgs";};
     # Pinned as of 2026-04-07 07:00
     nixos-hardware.url = "github:NixOS/nixos-hardware/c775c2772ba56e906cbeb4e0b2db19079ef11ff7";
-    i915-sriov-dkms = { # Pinned as of 2026-04-07 07:00, tag: 2026.03.05
-      url = "github:strongtz/i915-sriov-dkms/2026.03.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Pinned as of 2026-05-04 18:35, tag: 2026.03.05
+    i915-sriov-dkms = {url = "github:strongtz/i915-sriov-dkms/2026.05.03"; inputs.nixpkgs.follows = "nixpkgs";};
   };
   outputs = inputs: import ./main.nix inputs;
 }
