e73ae2a87ceebf433fdb2507fc5e318e29239dcc
Proteus Qian <sudaku233@outlook.com>
2026-05-04 17:20:09 +0800

    ✨ feat(darwin): auto-trust self-signed CA in macOS System Keychain on activation
    - add `postActivation` script to `modules/darwin/stew.nix` that installs `proteus_ca.pub.pem` as a trusted root in `/Library/Keychains/System.keychain` if absent
    - use `openssl x509` to extract the disk fingerprint and `/usr/bin/security find-certificate` to look up the Keychain fingerprint
    - compare SHA-1 fingerprint of the on-disk certificate against the Keychain entry and replace it if the certificate has been renewed
    
    Signed-off-by: Proteus Qian <sudaku233@outlook.com>

--- modules/darwin/stew.nix
+++ modules/darwin/stew.nix
@@ -151,4 +151,33 @@ in {
     shell = pkgs.zsh;
   };
   ## END users.nix
+  # `nix-darwin` only executes hardcoded list of script names during the rebuild process
+  # https://github.com/nix-darwin/nix-darwin/issues/663
+  # system.activationScripts.trust_custom_ca.text = ''
+  system.activationScripts.postActivation.text = ''
+    ## BEGIN ACTIVATION SCRIPT trust_custom_ca.sh
+    echo "Checking self-signed CA in macOS System Keychain..."
+
+    CA_CERT="${myvars.secrets_dir}/proteus_ca.pub.pem"
+    CA_NAME="Homo home"
+
+    # 1. Get the SHA-1 fingerprint of current self-signed certificate file on disk
+    # We use awk to just grab the hex part and remove the colons so it matches macOS output
+    DISK_FINGERPRINT=$(${lib.getExe pkgs.openssl} x509 -in "$CA_CERT" -noout -fingerprint -sha1 | awk -F '=' '{print $2}' | tr -d ':')
+
+    # 2. Lookup SHA-1 fingerprint of self-signed certificate in the macOS Keychain by its name
+    KEYCHAIN_FINGERPRINT=$(/usr/bin/security find-certificate -c "$CA_NAME" -Z /Library/Keychains/System.keychain 2>/dev/null | grep "SHA-1 hash:" | awk '{print $3}')
+
+    if [ -z "$KEYCHAIN_FINGERPRINT" ]; then
+      echo "Certificate not found in Keychain. Installing..."
+      sudo /usr/bin/security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "$CA_CERT"
+    elif [ "$DISK_FINGERPRINT" != "$KEYCHAIN_FINGERPRINT" ]; then
+      echo "Certificate renewal detected! Updating Keychain..."
+      sudo /usr/bin/security delete-certificate -Z "$KEYCHAIN_FINGERPRINT" /Library/Keychains/System.keychain
+      sudo /usr/bin/security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "$CA_CERT"
+    else
+      echo "Current CA is already trusted and up to date."
+    fi
+    ## END ACTIVATION SCRIPT trust_custom_ca.sh
+  '';
 }
